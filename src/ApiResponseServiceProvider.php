3fc3d40693be60e66ec63ee8b4c81746198d3502
AnasMaimalee <anasment6@gmail.com>
2026-01-31 07:05:39 +0530

    first commit

---
+++ src/ApiResponseServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Maimalee\LaravelApiResponse;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiResponseServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
